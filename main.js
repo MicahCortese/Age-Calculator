dab4fc11423ee05774d926f46cd15a375eed1237
Micah Cortese <59982286+MicahCortese@users.noreply.github.com>
2025-02-17 01:08:39 -0500

    Add Luxon library as a dependency and implement basic structure for date handling

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 import Datepicker from '/node_modules/vanillajs-datepicker/js/Datepicker.js';
 
 const picker = document.querySelector('input[name="datepicker"]');
+const currentDate = new Date();
 const datepicker = new Datepicker(picker, {
-    format: 'mm-dd-yyyy'
+    format: 'mm-dd-yyyy',
+    maxDate: new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate())
 });
