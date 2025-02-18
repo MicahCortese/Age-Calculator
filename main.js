3365dc5ba00a95b51bc69aae10bbcb59bd807120
corcheesey <micahcortese@gmail.com>
2025-02-17 19:55:39 -0500

    Update datepicker and luxon imports to latest versions for improved functionality

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import { DateTime } from 'https://cdn.jsdelivr.net/npm/luxon@2.3.0/build/es6/luxon.min.js';
+import { DateTime } from 'https://cdn.jsdelivr.net/npm/luxon@2.3.0/build/es6/luxon.js';
 import Datepicker from 'https://cdn.jsdelivr.net/npm/vanillajs-datepicker@1.1.4/js/Datepicker.min.js';
-
+import Datepicker from 'https://cdn.jsdelivr.net/npm/vanillajs-datepicker@1.3.4/dist/js/Datepicker.js';
 
 // Initialize the datepicker, set the format to 'mm-dd-yyyy', and set the max date to the current date
 const picker = document.querySelector('input[name="datepicker"]');
